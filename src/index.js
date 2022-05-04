b0ba6601a0397f59b222ba99de436f3def1f5eb9
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-05-04 16:11:41 +0700

    install sass & normalize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./component/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
